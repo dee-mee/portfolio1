860eca5daeab0f6d1590f37bd63d2c74aee09b6d
dee-mee <derekmuriuki2@gmail.com>
2025-05-08 00:14:15 +0300

    changed files

--- portfolio/asgi.py
+++ portfolio/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-application = get_asgi_application()
+# This application object is used by any ASGI server configured to use this file.
+django_asgi_app = get_asgi_application()
+
+# Define the application for WhiteNoise
+application = WhiteNoise(
+    django_asgi_app,
+    # Root directory for static files
+    root=os.path.join(Path(__file__).resolve().parent.parent, 'static'),
+    # Prefix for static files
+    prefix='/static/',
+    # Disable directory listings
+    autorefresh=True,
+)
--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-application = get_wsgi_application()
+# This application object is used by any WSGI server configured to use this file.
+django_app = get_wsgi_application()
+
+# Define the application for WhiteNoise
+application = WhiteNoise(
+    django_app,
+    # Root directory for static files
+    root=os.path.join(Path(__file__).resolve().parent.parent, 'static'),
+    # Prefix for static files
+    prefix='/static/',
+    # Disable directory listings
+    autorefresh=True,
+)
